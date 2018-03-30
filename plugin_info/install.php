7b8aea62bc2ace2d7a1138f6c07640b362d65e79
Halbout Michael <michaelhalbout@gmail.com>
2018-03-30 09:11:25 +0200

    Update install.php

--- plugin_info/install.php
+++ plugin_info/install.php
@@ -5,18 +5,18 @@ function ChauffeEau_install(){
 function ChauffeEau_update(){
 	log::add('ChauffeEau','debug','Lancement du script de mise a jours'); 
 	foreach(eqLogic::byType('ChauffeEau') as $eqLogic){
-    if($eqLogic->getConfiguration('Activation')!=''){
-      $ActionOn[]['cmd']=$eqLogic->getConfiguration('Activation');
-      $ActionOn[]['enable']=true;
-      $eqLogic->setConfiguration('ActionOn',$ActionOn);
-      $eqLogic->setConfiguration('Activation','');
-    }
-    if($eqLogic->getConfiguration('Desactivation')!=''){
-      $ActionOff[]['cmd']=$eqLogic->getConfiguration('Desactivation');
-      $ActionOff[]['enable']=true;
-      $eqLogic->setConfiguration('ActionOff',$ActionOff);
-      $eqLogic->setConfiguration('Desactivation','');
-    }
+		if($eqLogic->getConfiguration('Activation')!=''){
+			$ActionOn[]['cmd']=$eqLogic->getConfiguration('Activation');
+			$ActionOn[]['enable']=true;
+			$eqLogic->setConfiguration('ActionOn',$ActionOn);
+			$eqLogic->setConfiguration('Activation','');
+		}
+		if($eqLogic->getConfiguration('Desactivation')!=''){
+			$ActionOff[]['cmd']=$eqLogic->getConfiguration('Desactivation');
+			$ActionOff[]['enable']=true;
+			$eqLogic->setConfiguration('ActionOff',$ActionOff);
+			$eqLogic->setConfiguration('Desactivation','');
+		}
 		$eqLogic->save();
 	}
 	log::add('ChauffeEau','debug','Fin du script de mise a jours');
