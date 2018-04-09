76db869c7f126305dc0184897775408c7e1308ee
Halbout Michael <michaelhalbout@gmail.com>
2018-04-09 11:03:21 +0200

    Update install.php

--- plugin_info/install.php
+++ plugin_info/install.php
@@ -5,7 +5,7 @@ function ChauffeEau_install(){
 function ChauffeEau_update(){
 	log::add('ChauffeEau','debug','Lancement du script de mise a jours'); 
 	foreach(eqLogic::byType('ChauffeEau') as $eqLogic){
-		if($eqLogic->getConfiguration('Activation')!=''){
+	/*	if($eqLogic->getConfiguration('Activation')!=''){
 			$ActionOn[]['cmd']=$eqLogic->getConfiguration('Activation');
 			$ActionOn[]['enable']=true;
 			$eqLogic->setConfiguration('ActionOn',$ActionOn);
@@ -16,11 +16,11 @@ function ChauffeEau_update(){
 			$ActionOff[]['enable']=true;
 			$eqLogic->setConfiguration('ActionOff',$ActionOff);
 			$eqLogic->setConfiguration('Desactivation','');
-		}
+		}*/
 		$eqLogic->save();
-		$cron = cron::byClassAndFunction('ChauffeEau', 'Chauffe', array('ChauffeEau_id' => $eqLogic->getId()));
+	/*	$cron = cron::byClassAndFunction('ChauffeEau', 'Chauffe', array('ChauffeEau_id' => $eqLogic->getId()));
 		if (is_object($cron)) 	
-			$cron->remove();
+			$cron->remove();*/
 		
 	}
 	log::add('ChauffeEau','debug','Fin du script de mise a jours');
