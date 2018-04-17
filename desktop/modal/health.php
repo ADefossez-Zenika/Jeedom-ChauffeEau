98a40bd8d89a40694756aab4096ac0b6846b98c1
Halbout Michael <michaelhalbout@gmail.com>
2018-04-17 09:04:53 +0200

    Update health.php

--- desktop/modal/health.php
+++ desktop/modal/health.php
@@ -22,9 +22,9 @@ foreach ($eqLogics as $eqLogic) {
 	cho '<td><span class="label label-info" style="font-size : 1em; cursor : default;">' . $eqLogic->getId() . '</span></td>';
 	echo '<td><span class="label label-info" style="font-size : 1em; cursor : default;">' . $eqLogic->getName() . '</span></td>';
 	$status = '<span class="label label-success" style="font-size : 1em;cursor:default;">{{OK}}</span>';
-	/*if ($eqLogic->getStatus('state') == 'nok') {
+	if ($eqLogic->getStatus('state') == 'nok') {
 		$status = '<span class="label label-danger" style="font-size : 1em;cursor:default;">{{NOK}}</span>';
-	}*/
+	}
 	echo '<td>' . $status . '</td>';
 	echo '<td>' . $eqLogic->getPuissance() . 'W</td>';
 	echo '<td><span class="label label-info" style="font-size : 1em;cursor:default;">' . $eqLogic->getStatus('lastCommunication') . '</span></td>';
